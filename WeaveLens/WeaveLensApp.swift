b69baf8ce7404c5f7a50435d826c7036ca0d19ff
Ismatulla Mansurov <ismatullamansurov@gmail.com>
2024-11-17 21:45:53 -0700

    Initial Commit

---
+++ WeaveLens/WeaveLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeaveLensApp.swift
+//  WeaveLens
+//
+//  Created by Varkhuman Mac on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeaveLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeaveLensTests/WeaveLensTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeaveLensTests.swift
+//  WeaveLensTests
+//
+//  Created by Varkhuman Mac on 11/17/24.
+//
+
+import Testing
+
+struct WeaveLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
